0bd43fca299d209ea38bdc8f08e587e56e57adac
Patrick Leong <patrickleong211@gmail.com>
2015-03-23 12:11:56 +0800

    Added 数盟support

--- src/com/umeng/editor/ChannelEditor.java
+++ src/com/umeng/editor/ChannelEditor.java
@@ -17,6 +17,8 @@ public class ChannelEditor {
 	
 	private String mChannelName = "UMENG_CHANNEL";
 	private String mChannelValue = "placeholder";
+
+	private String mAntiCheatingName = "application_source";
 	
 	private int namespace;
 	private int meta_data;
@@ -24,6 +26,8 @@ public class ChannelEditor {
 	private int attr_value;
 	private int channel_name;
 	private int channel_value = -1;
+	private int anti_cheating_name;
+	private int anti_cheating_value = -1;
 	
 	private AXMLDoc doc;
 	
@@ -42,19 +46,25 @@ public class ChannelEditor {
 		attr_name = sb.putString(NAME);
 		attr_value = sb.putString(VALUE);
 		channel_name = sb.putString(mChannelName);
+		anti_cheating_name = sb.putString(mAntiCheatingName);
 		
 		if(channel_value == -1){
 			channel_value = sb.addString(mChannelValue);//now we have a seat in StringBlock
+		}
+
+		if(anti_cheating_value == -1){
+			anti_cheating_value = sb.addString(mChannelValue);
 		}
 	}
 	
 	//put string to the seat
 	private void replaceValue(StringBlock sb){
 		sb.setString(channel_value, mChannelValue);
+		sb.setString(anti_cheating_value, mChannelValue);
 	}
 
 	//Second find&change meta-data's value or add a new one
-	private void editNode(AXMLDoc doc){
+	private void editNode(AXMLDoc doc, int nameIdx, int valueIdx){
 		BXMLNode application = doc.getApplicationNode(); //manifest node
 		List<BXMLNode> children = application.getChildren();
 		
@@ -63,19 +73,19 @@ public class ChannelEditor {
 		end:for(BXMLNode node : children){
 			BTagNode m = (BTagNode)node;
 			//it's a risk that the value for "android:name" maybe not String
-			if((meta_data == m.getName()) && (m.getAttrStringForKey(attr_name) == channel_name)){
+			if((meta_data == m.getName()) && (m.getAttrStringForKey(attr_name) == nameIdx)){
 					umeng_meta = m;
 					break end;
 			}
 		}
 		
 		if(umeng_meta != null){
-			umeng_meta.setAttrStringForKey(attr_value, channel_value);
+			umeng_meta.setAttrStringForKey(attr_value, valueIdx);
 		}else{
 			Attribute name_attr = new Attribute(namespace, attr_name, TypedValue.TYPE_STRING);
-			name_attr.setString( channel_name );
+			name_attr.setString( nameIdx );
 			Attribute value_attr = new Attribute(namespace, attr_value, TypedValue.TYPE_STRING);
-			value_attr.setString( channel_value );
+			value_attr.setString( valueIdx );
 			
 			umeng_meta = new BTagNode(-1, meta_data);
 			umeng_meta.setAttribute(name_attr);
@@ -87,8 +97,9 @@ public class ChannelEditor {
 	
 	public void commit() {
 		registStringBlock(doc.getStringBlock());
-		editNode(doc);
-		
+		editNode(doc, channel_name, channel_value);
+		editNode(doc, anti_cheating_name, anti_cheating_value);
+				
 		replaceValue(doc.getStringBlock());
 	}
 }
